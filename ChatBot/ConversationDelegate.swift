ae495c83104bce9fe1bb1dd9a61bf51838b8cde1
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-04 19:12:03 +0300

    Initial Commit

---
+++ ChatBot/ConversationDelegate.swift
@@ -0,0 +1,22 @@
+struct ConversationDelegate {
+    /// Creates a String in response to another String.
+    func responseTo(question: String) -> String {
+            let lowercasedQuestion = question.lowercased()
+            if lowercasedQuestion.hasPrefix("hello") {
+                return "Why, hello there!"
+            } else if lowercasedQuestion == "where are the cookies?" || lowercasedQuestion.contains("cookie"){
+                return "In the cookie jar"
+            } else if lowercasedQuestion.hasPrefix("where") {
+                return "To the North"
+            } else {
+                let defaultNumber = question.count % 3
+                if defaultNumber == 0 {
+                    return "That really depends"
+                } else if defaultNumber == 1 {
+                    return "Why are you asking me that thigs? i'm not a wikipedia!"
+                } else {
+                    return "Ask me again tomorrow"
+                }
+            }
+        }
+}
